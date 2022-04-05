086eff103c84ae4e73b47c78ca25a64dac8ba550
Shreyans <Shreyansraj3@gmail.com>
2022-04-06 02:55:33 +0530

    mod

--- Project2/index.js
+++ Project2/index.js
@@ -3,7 +3,7 @@ navbar = document.querySelector('.navbar')
 sidebar = document.querySelector('.sidebar')
 nav = document.querySelector('.nav')
 
-burger.addEventListener('click',()=>{
+burger.addEventListener('click', () => {
     sidebar.classList.toggle('v-class-responsive');
     nav.classList.toggle('v-class-responsive');
     navbar.classList.toggle('h-nav-responsive');
